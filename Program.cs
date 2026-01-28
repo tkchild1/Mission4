207cd5abf357ebf6584e702e95e5e00312f1d75a
Nathan Blatter <nathan.blatter@yahoo.com>
2026-01-28 14:29:25 -0700

    iswinner

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using Mission4;
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
-Console.WriteLine("Pick a square by typing a number 0-8:");
+Console.WriteLine("Pick a square by typing a number 1-9:");
 Console.WriteLine();
 Console.WriteLine("Player X will go first");
 string[] board = ["1","2","3","4","5","6","7","8","9"];
@@ -12,21 +12,22 @@ bool iswinner = false;
 
 
 
-while (iswinner);
+while (iswinner)
 {
     TTT.printBoard(board);
     Console.WriteLine("Player X, What is your selection?");
     xSelection = int.Parse(Console.ReadLine());
     TTT.updateBoard("X", xSelection, board);
     TTT.printBoard(board);
-    TTT.isWinner(board);
+   iswinner = TTT.isWinner(board);
     Console.WriteLine("Player O, What is your selection?");
     OSelection= int.Parse(Console.ReadLine());
     TTT.updateBoard("O",OSelection, board);
     TTT.printBoard(board);
-    TTT.isWinner(board);
+    iswinner = TTT.isWinner(board);
 
 
 }
+
  
 
